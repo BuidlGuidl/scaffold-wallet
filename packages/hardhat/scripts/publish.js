535fd09f8a916bfa2c70d2d3c58b3681d8975da2
ghostffcode <meremezeukah@gmail.com>
2022-03-19 23:48:31 -0500

    change export path for hardhat contracts

--- packages/hardhat/scripts/publish.js
+++ packages/hardhat/scripts/publish.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 
 const graphDir = "../subgraph";
 const deploymentsDir = "./deployments";
-const publishDir = "../react-app/src/contracts";
+const publishDir = "../expo-app/contracts";
 
 function publishContract(contractName, networkName) {
   try {
