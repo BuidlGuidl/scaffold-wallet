1b6bfe876a9f7f55f2e8ea2d141edda9f2f50548
daniel <danieljkhoo@gmail.com>
2022-04-07 15:47:01 +0800

    fix: switch to etherscan gastracker

--- packages/expo-app/hooks/GasPrice.js
+++ packages/expo-app/hooks/GasPrice.js
@@ -1,25 +1,27 @@
 import axios from "axios";
 import { usePoller } from "eth-hooks";
 import { useState } from "react";
+import { ETHERSCAN_KEY } from "../constants";
+import { ethers } from "ethers";
 
 export default function useGasPrice(targetNetwork, speed) {
   const [gasPrice, setGasPrice] = useState();
   const loadGasPrice = async () => {
-    if (targetNetwork.hasOwnProperty("gasPrice")) {
+    if (targetNetwork.gasPrice) {
       setGasPrice(targetNetwork.gasPrice);
     } else {
-        axios
-          .get("https://ethgasstation.info/json/ethgasAPI.json")
-          .then(response => {
-            const newGasPrice = response.data[speed || "fast"] * 100000000;
-            if (newGasPrice !== gasPrice) {
-              setGasPrice(newGasPrice);
-            }
-          })
-          .catch(error => console.log(error));
+      axios
+        .get("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=" + ETHERSCAN_KEY)
+        .then(response => {
+          const newGasPrice = ethers.utils.parseUnits(response.data.result["SafeGasPrice"], "gwei")
+          if (newGasPrice !== gasPrice) {
+            setGasPrice(newGasPrice);
+          }
+        })
+        .catch(error => console.log(error));
     }
   };
 
-  usePoller(loadGasPrice, 39999);
+  usePoller(loadGasPrice, 4200);
   return gasPrice;
 }
