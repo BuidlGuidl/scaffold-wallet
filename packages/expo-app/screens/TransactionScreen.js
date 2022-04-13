6f4df3814df032cfd83de746540bbe8ee99ab6e5
daniel <danieljkhoo@gmail.com>
2022-04-13 16:01:43 +0800

    refactor: transaction screen styles

--- packages/expo-app/screens/TransactionScreen.js
+++ packages/expo-app/screens/TransactionScreen.js
@@ -1,10 +1,10 @@
 
 
 import { useState, useEffect } from "react";
-import { Button, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { ethers } from "ethers";
 import { truncateAddress } from "../helpers/utils";
-import { PERSONAL_SIGN, SEND_TRANSACTION, SIGN, SIGN_TRANSACTION } from "../constants";
+import { PERSONAL_SIGN, SEND_TRANSACTION, SIGN_TRANSACTION } from "../constants";
 
 const TransactionScreen = (props) => {
     const { address, balance, price, gasPrice, pendingTransaction, walletConnectParams, network } = props
@@ -29,7 +29,7 @@ const TransactionScreen = (props) => {
     const transferCostInETH = Number(ethers.utils.formatEther(gasPrice * gas))
     const transferCostInUSD = (transferCostInETH * price).toFixed(2)
     let insufficientFunds = false
-    if (isTransaction && amount && gasPrice && balance) {
+    if (isTransaction && gasPrice && balance) {
         insufficientFunds = (Number(amount) + transferCostInETH) > formattedEthBalance
     }
 
@@ -59,10 +59,7 @@ const TransactionScreen = (props) => {
         await props.cancelTransaction()
     }
 
-
-    return <View
-        style={{ position: 'absolute', height: '100%', width: '100%', backgroundColor: "#fff", flexDirection: 'column', paddingHorizontal: 20 }}>
-
+    return <View style={{ position: 'absolute', height: '100%', width: '100%', backgroundColor: "#fff", flexDirection: 'column', paddingHorizontal: 20 }}>
         <Text style={{
             marginTop: 40,
             marginHorizontal: 32,
@@ -74,69 +71,69 @@ const TransactionScreen = (props) => {
         </Text>
 
         {/* WC URL and Details */}
-        <View style={{ marginTop: 16, width: '100%', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-            <Image style={{ width: 30, height: 30, marginRight: 8 }} source={{ uri: WCIcon }} />
-            <Text style={{ fontSize: 18, fontWeight: '500' }}>{WCUrl}</Text>
+        <View style={[styles.row, { flexDirection: 'row' }]}>
+            <Image style={{ width: 40, height: 40, marginRight: 8 }} source={{ uri: WCIcon }} />
+            <Text style={[styles.rowTitle]}>{WCUrl}</Text>
         </View>
 
         {/* Current Wallet and Balance */}
-        <View style={{ width: '100%', marginTop: 16, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <View style={[styles.row, { flexDirection: 'row', justifyContent: 'space-between' }]}>
             <View style={{ flexDirection: 'column' }}>
-                <Text style={{ fontSize: 16, fontWeight: '500' }}>Wallet</Text>
-                <Text style={{ marginTop: 4, fontSize: 18, }}>{displayWalletAddress}</Text>
+                <Text style={[styles.rowTitle]}>Wallet</Text>
+                <Text style={[styles.rowData]}>{displayWalletAddress}</Text>
             </View>
             <View style={{ flexDirection: 'column', alignItems: 'flex-end' }}>
-                <Text style={{ fontSize: 16, fontWeight: '500' }}>Balance</Text>
-                <Text style={{ marginTop: 4, fontSize: 18, }}>{formattedEthBalance.toFixed(2)} ETH</Text>
+                <Text style={[styles.rowTitle]}>Balance</Text>
+                <Text style={[styles.rowData]}>{formattedEthBalance.toFixed(2)} ETH</Text>
             </View>
         </View>
 
         {/* Display different details for transaction vs signing requests */}
         {isTransaction ?
             <>
-                {/* Transaction Data */}
-                <View style={{ width: '100%', marginTop: 16, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={{ fontSize: 16, fontWeight: '500' }}>Data</Text>
-                    <Text style={{ marginTop: 8, fontSize: 14 }}>{data}</Text>
-                </View>
-
                 {/* Contract Address and the current Network */}
-                <View style={{ marginTop: 20, width: '100%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <View style={[styles.row, { flexDirection: 'row', justifyContent: 'space-between' }]}>
                     <View style={{ flexDirection: 'column' }}>
-                        <Text style={{ fontSize: 16, fontWeight: '500' }}>Contract Address</Text>
-                        <Text style={{ marginTop: 4, fontSize: 18 }}>{contractAddress}</Text>
+                        <Text style={[styles.rowTitle]}>Contract Address</Text>
+                        <Text style={[styles.rowData]}>{contractAddress}</Text>
                     </View>
                     <View style={{ flexDirection: 'column', alignItems: 'flex-end' }}>
-                        <Text style={{ fontSize: 16, fontWeight: '500' }}>Network</Text>
-                        <Text style={{ marginTop: 4, fontSize: 18 }}>{network.name} </Text>
+                        <Text style={[styles.rowTitle]}>Network</Text>
+                        <Text style={[styles.rowData]}>{network.name} </Text>
                     </View>
+                </View>
+
+                {/* Transaction Data */}
+                <View style={[styles.row]}>
+                    <Text style={[styles.rowTitle]}>Data</Text>
+                    <Text style={[styles.rowData, { marginTop: 8, fontSize: 18, fontStyle: 'italic' }]}>{data}</Text>
                 </View>
 
                 {/* Transaction Value and Estimated Gas */}
-                <Text style={{ marginTop: 20, fontSize: 40, fontWeight: '500', textAlign: 'center' }}>{amount} ETH</Text>
-                <View style={{ marginTop: 12, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                    <Text style={{ fontSize: 22 }}>~{(amount * price).toFixed(2)} USD</Text>
-                    <Text style={{ fontSize: 16 }}>Est. Fee ${transferCostInUSD}</Text>
+                <Text style={{ marginTop: 20, fontSize: 44, fontWeight: '500', textAlign: 'center' }}>{amount} ETH</Text>
+                <View style={{ marginTop: 16, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <Text style={[{ fontSize: 24 }]}>~{(amount * price).toFixed(2)} USD</Text>
+                    <Text style={[{ fontSize: 20 }]}>Est. Fee ${transferCostInUSD}</Text>
                 </View>
             </> :
             <>
                 {/* Message Signing Data */}
-                <View style={{ width: '100%', marginTop: 16, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={{ fontSize: 16, fontWeight: '500' }}>Message</Text>
-                    <Text style={{ marginTop: 8, fontSize: 14 }}>{plainTextMessage != null ? plainTextMessage : messageForSigning}</Text>
+                <View style={styles.row}>
+                    <Text style={[styles.rowTitle]}>Message</Text>
+                    <Text style={{ marginTop: 8, fontSize: 18, fontStyle: 'italic' }}>{plainTextMessage != null ? plainTextMessage : messageForSigning}</Text>
                 </View>
             </>}
 
         <View style={{ marginTop: 48, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
             <TouchableOpacity style={{ backgroundColor: '#0084ff', paddingVertical: 16, borderRadius: 32, width: '100%' }}
-                disabled={insufficientFunds}
+                disabled={loading || insufficientFunds}
                 onPress={confirm}>
                 <Text style={{ fontSize: 21, fontWeight: '500', color: '#fff', textAlign: 'center' }}>
                     {insufficientFunds ? 'Insufficient funds' : 'Confirm'}
                 </Text>
             </TouchableOpacity>
             <TouchableOpacity style={{ paddingVertical: 16, width: '100%', marginTop: 8 }}
-                onPress={reject}>
+                onPress={loading || reject}>
                 <Text style={{ fontSize: 21, fontWeight: '500', color: '#0084ff', textAlign: 'center' }}>
                     Reject
                 </Text>
@@ -146,5 +143,17 @@ const TransactionScreen = (props) => {
     </View>
 
 }
+
+const styles = StyleSheet.create({
+    row: {
+        marginTop: 24, width: '100%', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
+    },
+    rowTitle: {
+        fontSize: 18, fontWeight: '600'
+    },
+    rowData: {
+        marginTop: 6, fontSize: 22,
+    }
+});
 
 export default TransactionScreen
